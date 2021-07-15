267d3d5df7107da20814a8aee74e8379884e4cfa
Francis_Benavides <si.francissbh14@uniandes.edu.ec>
2021-07-15 09:55:28 -0500

    Primer commit

---
+++ backend-my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '541ee49cd573af935f14df87acae5a23'),
+    },
+  },
+});
---
+++ backend-my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c10e3912-249c-45b9-a191-63ebdfdec38e'
+};
